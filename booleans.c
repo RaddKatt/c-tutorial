3c61a1f2c4257368f28808fe6eff2d39a41b3f50
Mike Radka <mikeradka@gmail.com>
2018-06-20 23:36:46 -0700

    Added booleans.c

---
+++ booleans.c
@@ -0,0 +1,17 @@
+/*
+This program demonstrates the use of boolean operators
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("1 == ( 1 || 0 ): %d\n", 1 == ( 1 ||  0));
+	printf("2 == ( 1 || 0 ): %d\n", 2 == ( 1 || 0 ));
+	printf("!( 1 || 0 ): %d\n", !( 1 || 0 ));		// NOT example	
+	printf("2 != ( 1 || 0 ): %d\n", 2 != ( 1 || 0 )); 	// Another NOT example
+	printf("!( 1 || (1 && 0) ): %d\n", !( 1 || (1 && 0) ));	// AND is evaluated before OR
+	printf("!(( 1 || 0) && 0): %d\n", !(( 1 || 0) && 0));  // Parenthesis are useful
+
+	return 0;
+}
