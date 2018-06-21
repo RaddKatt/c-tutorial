22589d80cfe5899c9630cbe02be71a2382c72001
Mike Radka <mikeradka@gmail.com>
2018-06-20 22:52:15 -0700

    Added comparisons.c

---
+++ comparisons.c
@@ -0,0 +1,18 @@
+/*
+Demonstrates some comparisons
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("2 == 1: %d\n", 2 == 1);
+	printf("1 == 1: %d\n", 1 == 1);
+	printf("5 > 4: %d\n", 5 > 4);
+	printf("5 < 4: %d\n", 5 < 4);
+	printf("5 >= 5: %d\n", 5 >= 5);
+	printf("5 <= 5: %d\n", 5 <= 5);
+	printf("5 != 4: %d\n", 5 != 4);
+
+	return 0;
+}
---
+++ reading-input.c
@@ -0,0 +1,17 @@
+/*
+A simple demonstration of reading user input
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int this_is_a_number;
+
+	printf("Please enter a number: ");
+	scanf("%d", &this_is_a_number);
+	printf("You entered: %d\n", this_is_a_number);
+	getchar();
+
+	return 0;
+}
