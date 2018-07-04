362cc5cce069a8b49f3ff7c63e3dd53aef593404
Mike Radka <mikeradka@gmail.com>
2018-07-03 21:25:38 -0700

    Began adding pointer syntax notes

---
+++ lesson-6-structures/c-pointer-syntax.c
@@ -0,0 +1,12 @@
+/*
+A pointer declaration is formatted like this:	<variable_type> *<pointer_name;>
+For example:					int *pointer_to_the_integer;		
+
+To declare a pointer, an asterisk * must immediately preceed the var name (ex: int *my_pointer)
+
+Declaration of one pointer & one regular integer:
+	int *my_pointer, non_pointer;
+
+Declaration of two pointers:
+	int *pointer1, *pointer2;
+*/
